754e1671501266371470958777ca0be2228e2e8e
Halyna Melnykova <galyamelnykova@gmail.com>
2022-11-17 23:02:48 +0200

    add current and logout

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const validateBody = require("./validateBody");
 const validateStatusBody = require("./validateStatusBody");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
 
 module.exports = {
   validateBody,
   validateStatusBody,
   isValidId,
+  authenticate,
 };
