31497fa345711e22bc36e1e6210b6a8c20f14189
Halyna Melnykova <galyamelnykova@gmail.com>
2022-11-27 18:14:58 +0200

    have mistake in not favorite contacts

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,13 +2,27 @@ const { Contact } = require("../../models/contact");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit: 5,
-  });
-  res.json(result);
+
+  if (!favorite) {
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+      skip,
+      limit: 5,
+    });
+
+    res.json(result);
+  }
+
+  const favoriteResults = await Contact.find(
+    { owner, favorite },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit: 5,
+    }
+  );
+  res.json(favoriteResults);
 };
 
 module.exports = listContacts;
